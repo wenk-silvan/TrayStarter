bf4d31792bd6f1585ebf3039008e1fb4d4a9a115
Silvan Wenk <silvan.wenk@ch.schindler.com>
2015-10-20 14:24:06 +0200

    Define structure of Command XML

---
+++ src/TrayStarter/TrayStarter/Commands/CommandItem.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CommandItem.cs" company="Schindler Elevator Ltd.">
+//    Copyright (c) 2015 Schindler Elevator Ltd. All rights reserved.
+//    
+//    This software is the confidential and proprietary information of
+//    Schindler Elevator Ltd. ("Confidential Information"). You shall not disclose
+//    such confidential information and shall use it only in accordance with
+//    the terms of the license agreement you entered into with Schindler Ltd.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrayStarter.Commands
+{
+   public class CommandItem
+   {  
+      [XmlElement("name")]
+      public string Name { get; set; }
+
+      [XmlElement("command")]
+      public string Command { get; set; }
+
+      [XmlAttribute("runas")]
+      public RunAsType RunAs { get; set; } 
+   }
+}
---
+++ src/TrayStarter/TrayStarter/Commands/RunAsType.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RunAsType.cs" company="Schindler Elevator Ltd.">
+//    Copyright (c) 2015 Schindler Elevator Ltd. All rights reserved.
+//    
+//    This software is the confidential and proprietary information of
+//    Schindler Elevator Ltd. ("Confidential Information"). You shall not disclose
+//    such confidential information and shall use it only in accordance with
+//    the terms of the license agreement you entered into with Schindler Ltd.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrayStarter.Commands
+{
+   public enum RunAsType
+   {
+      [XmlEnum("admin")]
+      Admin = 1,
+      [XmlEnum("user")]
+      User
+   }
+}
