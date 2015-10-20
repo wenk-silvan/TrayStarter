2e99a4654b66676ec42248fcfa1e162482495c63
Silvan Wenk <silvan.wenk@ch.schindler.com>
2015-10-20 16:23:14 +0200

    Read command data from XML and outputs the order

---
+++ src/TrayStarter/TrayStarter/Commands/CommandManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TrayStarter.Commands
+{
+   public class CommandManager
+   {
+      public IDictionary<string, CommandItem> commands;
+
+      private static CommandManager instance = null;
+
+      private CommandManager()
+      {
+         this.commands = this.GetCommands().ToDictionary(c => c.Name, c => c);
+      }
+
+      public static CommandManager Instance
+      {
+         get { return CommandManager.instance ?? (CommandManager.instance = new CommandManager()); }
+      }
+
+      public CommandItem this[string name]
+      {
+         get
+         {
+            try
+            {
+               return this.commands[name];
+            }
+            catch (KeyNotFoundException)
+            {
+               throw new TrayStarterException("The entered command was not found in the command list.");
+            }
+         }
+      }
+
+      private IList<CommandItem> GetCommands()
+      {
+         var serializer = new XmlSerializer(typeof(CommandList));
+         using (var stream = XmlReader.Create(TrayStarter.Properties.Settings.Default.CommandsFile))
+         {
+            return ((CommandList)serializer.Deserialize(stream)).Commands;
+         }
+      }
+   }
+}
---
+++ src/TrayStarter/TrayStarter/TrayStarterConsole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TrayStarter.Commands;
+
+namespace TrayStarter
+{
+   internal sealed class TrayStarterConsole
+   {
+      public void Run()
+      {
+         Console.WriteLine("Welcome to TrayStarter");
+         Console.WriteLine();
+
+         Console.WriteLine("What would you like to start?");
+         Console.Write("> ");
+         var eingabe = Console.ReadLine();
+         Console.WriteLine("Your Command: {0}", eingabe);
+
+         try {
+            var command = CommandManager.Instance[eingabe];
+            Console.WriteLine(command.Command);
+         }
+         catch(TrayStarterException ex) {
+            Console.WriteLine(ex.Message);
+         }
+
+         Console.ReadKey();
+      }
+   }
+}
